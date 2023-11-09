ed10990c8f2b2b5982d98ba6b7a5e8d1815a6e8e
Dmytro Danylkovych <ipz233_dds@student.ztu.edu.ua>
2023-11-09 17:43:41 +0200

    output x

---
+++ Sana01/LinearExpressions1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Write 4 numbers: ");
+int a, b, c, d, x, y, z, r;
+int.TryParse(Console.ReadLine(), out a);
+int.TryParse(Console.ReadLine(), out b);
+int.TryParse(Console.ReadLine(), out c);
+int.TryParse(Console.ReadLine(), out d);
+
+x = (a + 2 * b - c + d) / c * d + (a + b) / (c - d) - (a * a) / (b * b);
+
+Console.WriteLine(x);
